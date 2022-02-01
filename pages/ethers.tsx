cbc8971dd29a6ff7148b54dca8946d8fb1878804
JustinCast <justincastillovalladares@gmail.com>
2022-01-31 21:02:47 -0600

    feat: typechain implementation

--- pages/ethers.tsx
+++ pages/ethers.tsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 import { getEthersProvider } from "../config/ethers";
 import { ethers } from "ethers";
 
+import { HiIQAbi__factory } from "../types/ethers-contracts/factories/HiIQAbi__factory";
+
 const EthersPage = () => {
-  const [address, setAddress] = useState<string>();
+  const [address, setAddress] = useState<string>("");
   const [provider, setProvider] = useState<any>();
   const [signer, setSigner] = useState<any>();
   const [balance, setBalance] = useState<string>();
@@ -22,7 +24,20 @@ const EthersPage = () => {
         const _signer = provider.getSigner();
         setSigner(_signer);
         setAddress(await _signer.getAddress());
+
+        const hiiq = HiIQAbi__factory.connect(
+          "0xc03bcacc5377b7cc6634537650a7a1d14711c1a3",
+          provider
+        );
+
+        console.log(
+          await hiiq.get_last_user_slope(
+            "0xAe65930180ef4d86dbD1844275433E9e1d6311ED",
+            { gasLimit: 300000 }
+          )
+        );
       };
+
       initialize();
     }
   }, [provider]);
@@ -49,7 +64,7 @@ const EthersPage = () => {
       <br />
       <span>Balance: {balance}</span>
       <br />
-      <button onClick={() => handleSign(address)}>Send 1 eth</button>
+      <button onClick={handleSign}>Send 1 eth</button>
     </div>
   );
 };
