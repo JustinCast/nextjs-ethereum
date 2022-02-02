75ce0ceecdb66c07d9c29e0509618098dd78640c
JustinCast <justincastillovalladares@gmail.com>
2022-02-01 18:47:24 -0600

    feat: wagmi provider config && easy balance & address fetching

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useEffect, useState } from "react";
+import { Provider } from "wagmi";
+
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ethers } from "ethers";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [provider, setProvider] = useState<any>();
+
+  useEffect(() => {
+    if (typeof window !== "undefined")
+      setProvider(new ethers.providers.Web3Provider((window as any).ethereum));
+  }, []);
+
+  return (
+    <Provider autoConnect provider={provider}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/wagmi.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import { useAccount, useBalance } from "wagmi";
+
+const Wagmi = () => {
+  const [{ data: accountData }] = useAccount();
+  const [{ data: balanceData }, getBalance] = useBalance({
+    addressOrName:
+      accountData && accountData.address ? accountData.address : undefined,
+  });
+
+  useEffect(() => {
+    console.log(accountData);
+  }, [accountData]);
+
+  useEffect(() => {
+    console.log(balanceData);
+  }, [balanceData]);
+
+  return (
+    <div>
+      {accountData ? accountData.address : "Loading"}
+      <br />
+      {balanceData?.formatted && balanceData.symbol ? (
+        <span>
+          {balanceData.formatted}: {balanceData.symbol}
+        </span>
+      ) : null}
+    </div>
+  );
+};
+
+export default Wagmi;
