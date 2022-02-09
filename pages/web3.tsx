97e6a7c77589e88f36288c916c4ba084b19b53ff
JustinCast <justincastillovalladares@gmail.com>
2022-02-08 20:21:28 -0600

    feat: add contract instance

--- pages/web3.tsx
+++ pages/web3.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { hiiqAbi } from "../abi/hiiq.abi";
 import { web3 } from "../config/web3";
 
 const Web3Page = () => {
   const [address, setAddress] = useState<string>();
+  const [contract, setContract] = useState<any>();
 
   const handleSign = async (address: string | undefined) => {
     await web3.eth
@@ -17,9 +19,25 @@ const Web3Page = () => {
   useEffect(() => {
     const getAccounts = async () =>
       setAddress((await web3.eth.getAccounts())[0]);
-
     getAccounts();
+
+    // create a contract instance
+    setContract(
+      new web3.eth.Contract(
+        hiiqAbi as any,
+        "0xC03bCACC5377b7cc6634537650A7a1D14711c1A3"
+      )
+    );
   }, [web3]);
+
+  useEffect(() => {
+    if (contract && address) {
+      const callContractFn = async () => {
+        console.log(await contract.methods.locked__end(address).call());
+      };
+      callContractFn();
+    }
+  }, [contract, address]);
 
   return (
     <div>
